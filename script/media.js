264c05acd860bb3dd992f3c06ae0d2ec31c9b18a
behruzbeek <behruzjalolov13@mail.com>
2024-11-07 13:14:54 +0500

    Js file

--- script/media.js
+++ script/media.js
@@ -9,8 +9,8 @@
      
 //  }
 
-const Sans = document.querySelector(".Sans-a")
-const Naver = document.querySelector(".navbar")
+// const Sans = document.querySelector(".Sans-a")
+// const Naver = document.querySelector(".navbar")
 
 // function Sansserif() {
 //     if (Sans) {
@@ -19,3 +19,33 @@ const Naver = document.querySelector(".navbar")
 
     
 // }
+
+
+const container = document.querySelector('.Container')
+ const fonts = document.querySelector('.form-select')
+ const Herro = document.querySelector('.site-Herro')
+ console.log(fonts)
+ fonts.addEventListener('change', () => {
+     console.log(fonts.value)
+     let font = fonts.value.toLowerCase()
+     console.log(font)
+     if (fonts.value === "SansSerif") {
+         container.style.fontFamily = "Inter"
+     }else if (fonts.value  === "Serif") {
+         container.style.fontFamily = "Lora"
+     }else {
+         container.style.fontFamily = "Mono"
+     }
+    // if (fonts.value === "SansSerif") {
+    //     Herro.style.fontFamily = "Inter"
+    // }else if (fonts.value === "Serif") {
+    //     Herro.style.fontFamily = "Lora"
+    // }else if (fonts.value === "Mono") {
+    //     Herro.style.fontFamily = "Mono"
+
+    // }
+
+     
+ })
+
+ 
