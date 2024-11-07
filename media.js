d449bb61ea7b0ce437ae6728a83b4b21e57c3eb0
behruzbeek <behruzjalolov13@mail.com>
2024-11-07 10:18:52 +0500

    Finish Dictonary

--- media.js
+++ media.js
@@ -1,10 +1,21 @@
-const Btn = document.querySelector(".Snas-a")
- function toggleMenu() {
-     var menu = document.getElementById("menu")
-     if (menu.style.display === "none") {
-         menu.style.display = "block";
-     }else {
-         menu.style.display = "none"
-     }
+// const Btn = document.querySelector(".Snas-a")
+//  function toggleMenu() {
+//      var menu = document.getElementById("menu")
+//      if (menu.style.display === "none") {
+//          menu.style.display = "block";
+//      }else {
+//          menu.style.display = "none"
+//      }
      
- }
+//  }
+
+const Sans = document.querySelector(".Sans-a")
+const Naver = document.querySelector(".navbar")
+
+// function Sansserif() {
+//     if (Sans) {
+//         Sans:active
+//     }
+
+    
+// }
